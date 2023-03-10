59e0c495039f839aef6b7273dbcad65e1e99c67b
swOk93 <91178260+swOk93@users.noreply.github.com>
2023-03-10 19:21:51 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,7 +1,29 @@
 package converter // Do not delete this line
 
-fun converter(decimalNum: Int, base: Int): String {
-    var quotient = decimalNum
+fun converterTo(): Int {
+    println("Enter source number:")
+    var quotient = readln()
+    println("Enter source base:")
+    val base = readln().toDouble()
+    var res = 0.0
+    repeat(quotient.length) {
+        if (base > 9 && it != quotient.length) {
+            val n = quotient[it].uppercase().code - 45
+            res += n * Math.pow(base, (quotient.length-it-1).toDouble())
+        }
+        else if (it != quotient.length) {
+            if (quotient[it].digitToInt() in 0..9) res += quotient[it].digitToInt() * Math.pow(base, (quotient.length-it-1).toDouble())
+        }
+        else res += quotient[it].digitToInt()
+    }
+    return res.toInt()
+}
+
+fun converterFrom(): String {
+    println("Enter number in decimal system:")
+    var quotient = readln().toInt()
+    println("Enter target base:")
+    val base = readln().toInt()
     var res = ""
     while (quotient != 0) {
         val rem = quotient % base
@@ -9,12 +31,18 @@ fun converter(decimalNum: Int, base: Int): String {
         else if (rem in 10..35) res = (55 + rem).toChar() + res
         quotient = quotient / base
     }
+    if (res == "") return "0"
     return res
 }
 
 fun main() {
-    println("Enter number in decimal system:")
-    val decimalNum = readln().toInt()
-    println("Enter target base:")
-    println("Conversion result: ${converter(decimalNum, readln().toInt())}")
+    var action = ""
+    while (action != "/exit") {
+        println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)")
+        action = readln()
+        when (action) {
+            "/to" -> println("Conversion to decimal result: ${converterTo()}")
+            "/from" -> println("Conversion result: ${converterFrom()}")
+        }
+    }
 }
