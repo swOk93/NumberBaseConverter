0dec616e3af2d58e3b848f1c77ec8b7b3f993218
swOk93 <91178260+swOk93@users.noreply.github.com>
2023-03-16 13:36:29 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,65 +1,57 @@
 package converter // Do not delete this line
 
+import java.math.BigInteger
+
 var sourse = 0
 var target = 0
 
-fun converterTo(): Int {
+fun converterTo(quot: String): String {
     println("Enter source number:")
-    var quotient = readln()
+    var quotient = quot
     println("Enter source base:")
-    val base = readln().toDouble()
-    var res = 0.0
+    var res = BigInteger("0")
     repeat(quotient.length) {
-        if (base > 9 && !quotient[it].isDigit()) {
+        if (sourse > 9 && !quotient[it].isDigit()) {
             val n = quotient[it].uppercaseChar().code - 55
-            res += n * Math.pow(base, (quotient.length-it-1).toDouble())
+            res += (n * Math.pow(sourse.toDouble(), (quotient.length-it-1).toDouble())).toInt().toBigInteger()
         }
         else if (it != quotient.length) {
-            if (quotient[it].digitToInt() in 0..9) res += quotient[it].digitToInt() * Math.pow(base, (quotient.length-it-1).toDouble())
+            if (quotient[it].digitToInt() in 0..9) res += (quotient[it].digitToInt() * Math.pow(sourse.toDouble(), (quotient.length-it-1).toDouble())).toInt().toBigInteger()
         }
     }
-    return res.toInt()
+    return res.toString()
 }
 
-fun converterFrom(): String {
-    println("Enter number in decimal system:")
-    var quotient = readln().toInt()
+fun converterFrom(quotient: String): String {
+    var quotient = quotient.toBigInteger()
+    if (sourse != 10) quotient = converterTo(quotient.toString()).toBigInteger()
     println("Enter target base:")
-    val base = readln().toInt()
     var res = ""
-    while (quotient != 0) {
-        val rem = quotient % base
+    while (quotient != BigInteger.ZERO) {
+        val rem = (quotient % target.toBigInteger()).toInt()
         if (rem in 0..9) res = rem.toString() + res
         else if (rem in 10..35) res = (55 + rem).toChar() + res
-        quotient = quotient / base
+        quotient = quotient / target.toBigInteger()
     }
     if (res == "") return "0"
     return res
 }
 
-fun converter(): String {
-    var quotient = 0
+fun converter() {
+    var quotient = ""
     while (quotient != "/back") {
         println("Enter number in base ${sourse} to convert to base ${target} (To go back type /back)")
-        var quotient = readln()
+        var quotient = "abcde"
         if (quotient != "/back") {
+            // if (!isNumeric(quotient)) continue
+            if (target == 10) println("Conversion result:${converterTo(quotient)}")
+            else println("Conversion result:${converterFrom(quotient)}")
         }
+    }
 }
 
 fun main() {
-    var action = ""
-    while (action != "/exit") {
-        println("Enter two numbers in format: {source base} {target base} (To quit type /exit)")
-        action = readln()
-        if (action != "/exit") {
-            val list = List(2) { action.split(' ') }
-            sourse = list[0]
-            target = list[1]
-            converter()
-        }
-        // when (action) {
-        //     "/to" -> println("Conversion to decimal result: ${converterTo()}")
-        //     "/from" -> println("Conversion result: ${converterFrom()}")
-        // }
-    }
+    sourse = 36
+    target = 10
+    converter()
 }
