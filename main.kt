03f7d9731efb22a713d8515f4bd47f936ea60ba4
swOk93 <91178260+swOk93@users.noreply.github.com>
2023-03-16 14:32:55 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -6,9 +6,7 @@ var sourse = 0
 var target = 0
 
 fun converterTo(quot: String): String {
-    println("Enter source number:")
     var quotient = quot
-    println("Enter source base:")
     var res = BigInteger("0")
     repeat(quotient.length) {
         if (sourse > 9 && !quotient[it].isDigit()) {
@@ -25,7 +23,6 @@ fun converterTo(quot: String): String {
 fun converterFrom(quotient: String): String {
     var quotient = quotient.toBigInteger()
     if (sourse != 10) quotient = converterTo(quotient.toString()).toBigInteger()
-    println("Enter target base:")
     var res = ""
     while (quotient != BigInteger.ZERO) {
         val rem = (quotient % target.toBigInteger()).toInt()
@@ -39,13 +36,15 @@ fun converterFrom(quotient: String): String {
 
 fun converter() {
     var quotient = ""
-    println("Enter number in base ${sourse} to convert to base ${target} (To go back type /back)")
-    quotient = readln()
-    if (quotient != "/back") {
+    while (quotient != "/back") {
+        println("Enter number in base ${sourse} to convert to base ${target} (To go back type /back)")
+        quotient = readln()
+        if (quotient != "/back") {
             // if (!isNumeric(quotient)) continue
-        if (target == 10) println("Conversion result:${converterTo(quotient)}")
-        else println("Conversion result:${converterFrom(quotient)}")
-    }
+            if (target == 10) println("Conversion result: ${converterTo(quotient)}")
+            else println("Conversion result: ${converterFrom(quotient)}")
+        }
+    }   
 }
 
 fun main() {
@@ -54,9 +53,9 @@ fun main() {
         println("Enter two numbers in format: {source base} {target base} (To quit type /exit)")
         action = readln()
         if (action != "/exit") {
-            val list = List(2) { action.split(' ') }
-            sourse = list[0].toInt()
-            target = list[1].toInt()
+            val list = action.split(' ').map { it.toInt() }
+            sourse = list[0]
+            target = list[1]
             converter()
         }
     }
